3acb508646993244e9e1491b3c1bfe517914cd7a
haisapan <haisapan@hotmail.com>
2016-10-20 11:15:35 +0800

    add the errorHandler

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -9,13 +9,18 @@ faker.locale = "zh_CN";
 
 var app = express();
 app.use(cors()); //Enable CORS
-app.use(bodyParser.json({limit: '1mb'}));
-app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(bodyParser.json({
+    limit: '1mb'
+}));
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '1mb'
+}));
 app.use(require('method-override')());
 
 /**Init the dataSource --Begin */
 
-var total = 3;
+var total = 43;
 var dataSource = [];
 for (let i = 0; i < total; i++) {
     var item = {
@@ -40,7 +45,7 @@ app.get('/', function (req, res) {
 
 /**Get Table Data */
 app.get('/api/coretable', function (req, res) {
-       var queryList= _.take(_.drop(dataSource, req.query.pageSize* (req.query.page-1)), req.query.pageSize);
+        var queryList = _.take(_.drop(dataSource, req.query.pageSize * (req.query.page - 1)), req.query.pageSize);
         res.send({
             total: total,
             data: queryList
@@ -48,31 +53,42 @@ app.get('/api/coretable', function (req, res) {
     })
     .post('/api/coretable', function (req, res) {
         // console.log(req, res);
-        var newItem= req.body;
-    
+        var newItem = req.body;
+
         dataSource.push(newItem);
-        newItem.id= faker.random.number();
+        newItem.id = faker.random.number();
         res.send(newItem);
     })
-    .put('/api/coretable', function (req, res) {
+    .put('/api/coretable', function (req, res, next) {
         console.log(req, res);
-        var updateItem= req.body;
-        
-        var existedOne=_.find(dataSource, function(item){
-            return item.NO==updateItem.NO;
+        var updateItem = req.body;
+
+        var existedOne = _.find(dataSource, function (item) {
+            return item.NO == updateItem.NO;
         });
+        if(!existedOne){
+            next("not found the item need be update!");
+        }
         _.assign(existedOne, updateItem);
         // existedOne=updateItem;
         // dataSource.push(newItem);
         res.send(existedOne);
     })
-    .delete('/api/coretable', function (req, res) {
+    .delete('/api/coretable', function (req, res, next) {
         console.log(req, res);
-        var deleteItem=req.body;
-        _.remove(dataSource, function(item){
-            return item.NO==deleteItem.NO;
-        })
-        res.send("OK");
+        var deleteItem = req.body;
+        var deleted=_.remove(dataSource, function (item) {
+            return item.NO == deleteItem.NO;
+        });
+        if(!deleted){
+            next("delete failed!");
+        }
+        res.sendStatus(200);
     });
 
-app.listen(7001);
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+
+    app.listen(7001);
